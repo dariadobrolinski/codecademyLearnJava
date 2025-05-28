e2a30c6585eaaae5e8b5c5efc9e952fff93e938d
Daria Dobrolinski <dsdobrolinski@gmail.com>
2025-05-28 10:34:12 -0400

    Add initial Tree class and README for Java learning projects

---
+++ Tree.java
@@ -0,0 +1,23 @@
+public class Tree {
+  public static void main(String[] args) {
+    // this code will print a tree
+    System.out.println("I'm Daria and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+
+    System.out.println("Tree #1:");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");   
+
+    System.out.println("~~~~~~~~~~");
+
+    System.out.println("Tree #2:");
+    System.out.println(" * ** * ");
+    System.out.println(" ****** ");
+    System.out.println("  ****  ");
+    System.out.println("   **   ");
+    System.out.println("   **  ");
+  }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Codecademy Java Learning Projects
+
+This repository contains my work as I progress through the [Codecademy Learn Java course](https://www.codecademy.com/enrolled/courses/learn-java). It includes various mini projects and exercises that I complete as part of the curriculum. Each project is designed to help reinforce key Java programming concepts and build practical coding skills.
+
+As I advance through the course, I will continue to add new projects and code samples to this repository. This is a personal learning journey, and the code here reflects my growth and understanding of Java.
+
+Feel free to explore the code and follow along with my progress!
