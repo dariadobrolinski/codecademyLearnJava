e5187d66012a747cb68f0b7e777550c8421e5321
Daria Dobrolinski <dsdobrolinski@gmail.com>
2025-06-12 14:56:26 -0400

    Refactor Calculator class to move main method to the top for better readability

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,16 @@
 // simple calculator class
 public class Calculator {
-  
-  public Calculator() {
 
+public static void main(String[] args) {
+  Calculator myCalculator = new Calculator();
+  System.out.println(myCalculator.add(5,7));
+  System.out.println(myCalculator.subtract(45, 11));
+  System.out.println(myCalculator.multiply(8, 3));
+  System.out.println(myCalculator.divide(12, 5));
+  System.out.println(myCalculator.modulo(15, 6));
   }
+  
+  public Calculator() {}
 
   public int add(int a, int b) {
     return a + b;
@@ -23,15 +30,6 @@ public class Calculator {
 
   public int modulo(int a, int b) {
     return a % b;
-  }
-
-  public static void main(String[] args) {
-  Calculator myCalculator = new Calculator();
-  System.out.println(myCalculator.add(5,7));
-  System.out.println(myCalculator.subtract(45, 11));
-  System.out.println(myCalculator.multiply(8, 3));
-  System.out.println(myCalculator.divide(12, 5));
-  System.out.println(myCalculator.modulo(15, 6));
   }
 
 }
