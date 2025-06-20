72e58268ff610c673f39919b1f76b033076d2708
Daria Dobrolinski <dsdobrolinski@gmail.com>
2025-06-20 12:41:51 -0400

    fix: fix small errors in FizzBuzz class

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,18 +1,17 @@
 public class FizzBuzz {
   public static void main(String[] args) {
-    for (int i = 1; i < 100; i++) {
+    for (int i = 1; i <= 100; i++) {
 
       if (i % 3 == 0 && i % 5 == 0) {
         System.out.println("FizzBuzz");
-        continue;
       }
-      
-      if (i % 3 == 0) {
+
+      else if (i % 3 == 0) {
         System.out.println("Fizz");
         continue;
       }
 
-      if (i % 5 == 0) {
+      else if (i % 5 == 0) {
         System.out.println("Buzz");
         continue;
       }
